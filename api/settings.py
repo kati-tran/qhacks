da97307f9855f834a95d796b93c333dc76382b08
sandra-soto <sandra909@live.com>
2021-11-21 02:24:01 -0800

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'https://qhacks-backend.herokuapp.com',
     'http://localhost:8081',
