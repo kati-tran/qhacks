fc39f2025ad299b35a8f8b8e61c87dd256484ffa
kati-tran <katit1@uci.edu>
2021-11-20 21:57:32 -0800

    app init setup

---
+++ qhacks2021/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'qhacks2021',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
