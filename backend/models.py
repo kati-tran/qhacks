b85fe3f1047f36075968f7cf6a4c3bf84fdb2c73
sandra-soto <sandra909@live.com>
2021-11-21 05:20:48 -0800

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=70, blank=False, default='')
-    description = models.CharField(max_length=500,blank=False, default='')
+    description = models.CharField(max_length=2000,blank=False, default='')
     pay_email = models.CharField(max_length=70, blank=False, default='')
     createDate = models.DateTimeField(default=None, blank=True, null=True)
     type = models.IntegerField(default=1)
